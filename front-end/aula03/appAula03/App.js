424678dacc9edb1d4066f97fad126be787dcc342
Rendell Arruda <imersao.grupo7@going2.com.br>
2022-08-24 17:41:53 -0300

    atualiza pasta apos aula 04 de front

--- front-end/aula03/appAula03/App.js
+++ front-end/aula03/appAula03/App.js
@@ -1,30 +1,71 @@
 import React, { Component } from 'react'
 import {
   View,
+  Pressable,
   Button,
   SafeAreaView,
   Text,
+  TouchableOpacity,
   TextInput,
   StyleSheet,
   Image,
   Alert
 } from 'react-native'
-
+import FontAwesome from '@expo/vector-icons/FontAwesome'
+// https://fontawesome.com/icons
 class App extends Component {
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <Button title="Clique-Me" onPress={() => Alert.alert('Olá!')} />
-      </SafeAreaView>
+      <View style={styles.container}>
+        <FontAwesome.Button
+          name="facebook"
+          backgroundColor="#3b5998"
+          onPress={() => alert('Hello, world!')}
+        >
+          Login com Facebook
+        </FontAwesome.Button>
+        <Text> </Text>
+        <FontAwesome.Button
+          name="apple"
+          backgroundColor="black"
+          onPress={() => alert('Hello, world!')}
+        >
+          Login com Apple
+        </FontAwesome.Button>
+        <Text> </Text>
+        <FontAwesome.Button
+          name="whatsapp"
+          backgroundColor="green"
+          onPress={() => alert('Hello, world!')}
+        >
+          Enviar whatsapp
+        </FontAwesome.Button>
+      </View>
     )
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 40
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'blue'
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white'
   }
 })
-
 export default App
