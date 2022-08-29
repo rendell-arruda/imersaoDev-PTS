0bf6f1cf81d7bf687efe80d40722a0a193e3e666
Rendell Arruda <imersao.grupo7@going2.com.br>
2022-08-29 17:09:13 -0300

    add arquivos da aula5 de front-end

--- back-end/aula05/terceiraAPI.js
+++ back-end/aula05/terceiraAPI.js
@@ -1,80 +1,65 @@
 // em javascript, em node.js
 // require significa que estou RRRRRREQUERENDO / importando o modulo
 var http = require('http')
-var url = require('url');
-
+// modulo inbuit do node
+//responsavel por fazer as tratativas de quebrar a url em pedacos menores conforme nossa necessidade de dado
+var url = require('url')
 
 // nesse momento ja temos o modulo
 // vamos agora simular um servidor
 
-
-
 // funcao pra representar o servidor
 // parametros eh o que vai dentro do parentese
-function server(request, response){
+function server(request, response) {
+  if (request.method === 'GET') {
+    // pegando as partes da URL
+    //o primeiro parametro request.url é a url completa
+    //o segundo parametro é para indicar que vou precisar da query/parametros de pesquisa depois do ponto de '?'
+    var parts = url.parse(request.url, true)
 
-    if(request.method === 'GET'){
+    // comparando o pathname é o caminho de contexto
+    if (parts.pathname === '/imersaodev') {
+      //query é o que vai pegar os de parametro de 'pesquisa'
+      var query = parts.query
 
-        // pegando as partes da URL
-        var parts = url.parse(request.url, true)
+      // definimos um parametro de pesquis chamado param1
+      // vamos imprimi-lo no console, que é o terminal 'preto' que roda do lado do servidor
+      console.log(query.nome)
 
-        // pathname é o caminho de contexto
-        if(parts.pathname === '/imersaodev'){
+      // pode ser tambem
+      console.log(query.nome)
 
-            //query é o que vai de parametro
- de 'pesquisa'
-            var query = parts.query
+      // resposta com status 200 deu bao - requisicao deu bao, fez tudo certin
+      response.writeHead(200, { 'Content-Type': 'application/json' })
 
-            // definimos um parametro de pesquis chamado param1
-            // vamos imprimi-lo no console, que é o terminal 'preto' que roda do lado do servidor
-            console.log(query.param1)
+      // conteudo da resposta
+      response.write(query.nome + ' ' + query.idade)
 
-            // pode ser tambem
-            console.log(query["param1"])
-        
-            // resposta com status 200 deu bao - requisicao deu bao, fez tudo certin
-            response.writeHead(200, {'Content-Type': 'application/json'})
+      // fechando a resposta
+      response.end()
+    } else if (request.url === '/imersao2') {
+      // resposta com status 200 deu bao - requisicao deu bao, fez tudo certin
+      response.writeHead(200, { 'Content-Type': 'application/json' })
 
-            // conteudo da resposta
-            response.write(query.param1 + ' ' + query.param2)
+      // conteudo da resposta
+      response.write('Imersao 2')
 
-            // fechando a resposta
-            response.end()
-        }
-
-        else if(request.url === '/imersao2'){
-        
-            // resposta com status 200 deu bao - requisicao deu bao, fez tudo certin
-            response.writeHead(200, {'Content-Type': 'application/json'})
-
-            // conteudo da resposta
-            response.write('Imersao 2')
-
-            // fechando a resposta
-            response.end()
+      // fechando a resposta
+      response.end()
+    } else {
+      response.writeHead(404)
+      response.write('nao encontrado!')
+      response.end()
     }
-
-    else{
-        response.writeHead(404)
-        response.write('nao encontrado!')
-        response.end();
-
-    }
-    
-
-   }
-
-   else if (request.method === "POST"){
-
-    response.writeHead(200, {'Content-Type': 'application/json'})
+  } else if (request.method === 'POST') {
+    response.writeHead(200, { 'Content-Type': 'application/json' })
 
     // conteudo da resposta
     response.write('POST AQUI')
 
     // fechando a resposta
     response.end()
-   }
-
+  }
 }
 
 // vou criar um servidor, passando como parametro a funcao q criei acima
